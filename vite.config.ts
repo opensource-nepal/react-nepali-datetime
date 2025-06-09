c541af3af9eb6bfe704593d4f74d6c7b6d05140b
Subesh Yadav <41501661+subeshyadav3@users.noreply.github.com>
2025-06-09 22:29:03 +0545

    add

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './react-nepali-datetime/',
   plugins: [
     react(),
     dts({
